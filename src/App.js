f5e2ad45beae3fbf6d16befbe97f05f36cd6b4f2
R3mpty <R3mpty@gmail.com>
2021-08-23 13:56:34 -0400

    Stuck on shuffling the orders
    The game works properly now but there are issues with the shuffling logic

--- src/App.js
+++ src/App.js
@@ -3,49 +3,82 @@ import Cards from './components/Cards'
 import './AppStyles.css'
 
 
+/*
+Notes:
+1. Create a array of all the cards
+2.
+3. Generate a random number
+4. Create a newly shuffled array with the random numbers as indexes
+5.
+
+*/
+
 function App() {
 
   console.warn = () => {};
 
-  
-  let [CarriedSkull, setCarriedSkull] = useState(
-    {
+  // const cards = [
+  //   'CarriedSkull',
+  //   'DualKnife',
+  //   'DarkPichu',
+  //   'LuckyCat',
+  //   'SkullCat',
+  //   'TheCircleOfCats'
+  // ]
+
+  // const defaultState = cards.reduce((newState, cardName) => (
+  //   {...newState, [cardName]: 0}), {});
+
+  // console.log(defaultState);
+
+  // const [selectedCards, setSelectedCards] = useState(defaultState);
+
+  // const increaseCount = (name) => {
+  //   setSelectedCards(
+  //     {
+  //       ...selectedCards,
+  //       [name]: selectedCards[name] + 1
+  //     }
+  //   )
+  // }
+
+
+  let CarriedSkull ={
       name: "CarriedSkull",
       clicked: 0,
-      // test : useRef(null)
-    }
-  )
-  let [DualKnife, setDualKnife] = useState(
+  }
+
+  let DualKnife = 
     {
       name: "DualKnife",
       clicked: 0,
     }
-  )
-  let [DarkPichu, setDarkPichu] = useState(
+  
+  let DarkPichu =
     {
       name: "DarkPichu",
       clicked: 0,
     }
-  )
-  let [LuckyCat, setLuckyCat] = useState(
+  
+  let LuckyCat = 
     {
       name: "LuckyCat",
       clicked: 0,
     }
-  )
-  let [SkullCat, setSkullCat] = useState(
+  
+  let SkullCat =
     {
       name: "SkullCat",
       clicked: 0,
     }
-  )
-  let [TheCircleOfCats, setTheCircleOfCats] = useState(
+  
+  let TheCircleOfCats = 
     {
       name: "TheCircleOfCats",
       clicked: 0,
     }
-  )
 
+  // I don't fully understand the whole reduce thing so I am going to stick with what I had, if my interpretation is correct, the only thing that is different here is that there is a clicked variable
   let [listOfCards, setListOfCards] = useState([
     CarriedSkull,
     DualKnife,
@@ -54,7 +87,7 @@ function App() {
     SkullCat,
     TheCircleOfCats,
   ])
- 
+
   // stuck on randomizing the order
   let [order, setOrder] = useState(Math.floor((Math.random() * 6) + 1));
   let [score, setScore] = useState(0);
@@ -63,15 +96,24 @@ function App() {
   useEffect(() => {
   }, [score, highScore]);
   
-  const randomizeOrder = (card) => {
-    //card.style.order = order; // card is not a dom element here, it is an object
+  // This should do the same things as the two function given
+  const randomizeOrder = (listOfCards) => {
+    let newArray = [];
+
+    for (let i = 0; i < 6; i++){
+      newArray[order] = listOfCards[i];
+      setOrder(order = (Math.floor((Math.random() * 6) + 1)));
+    }
+
+    console.log(newArray)
+    return newArray;
 
   }
 
   const clickAndCheck = (card) => {
     card.clicked = card.clicked + 1; // not fully sure why I don't need setState here
     
-    randomizeOrder(card);
+    listOfCards = randomizeOrder(listOfCards);
 
     if (card.clicked > 1) {
       console.log("You Lose");
@@ -91,9 +133,7 @@ function App() {
     else{
       setScore(score + 1);
     }
-  }
-
- 
+  } 
 
   return (
 
@@ -108,17 +148,9 @@ function App() {
       </div>
 
       <div id="cardContainer">
-        <Cards  onClick = {() => {clickAndCheck(CarriedSkull)}} className = {randomizeOrder} name = "Carried Skull" src = "/imgs/CarriedSkull.png"/>
 
-        <Cards onClick = {() => {clickAndCheck(DarkPichu)}} className = {randomizeOrder}  name = "Dark Pichu" src = "/imgs/DarkPichu.png"/>
-
-        <Cards onClick = {() => {clickAndCheck(DualKnife)}} className = {randomizeOrder}  name = "Dual Knife" src = "/imgs/DualKnife.png"/>
-
-        <Cards onClick = {() => {clickAndCheck(LuckyCat)}} className = {randomizeOrder}  name = "Lucky Cat" src = "/imgs/LuckyCat.png"/>
-
-        <Cards onClick = {() => {clickAndCheck(SkullCat)}} className = {randomizeOrder}  name = "Skull Cat" src = "/imgs/SkullCart.png"/>
-
-        <Cards onClick = {() => {clickAndCheck(TheCircleOfCats)}} className ={randomizeOrder}   name ="TheCircleOfCats" src = "/imgs/TheCircle.png"/>
+        {/* Can you show me how you would pass the rest of the information in for the cards to display properly? */}
+        {listOfCards.map(card => (<Cards key = {card} />))}
       </div>
 
 
@@ -128,3 +160,17 @@ function App() {
 
 
 export default App;
+
+
+// archived:
+{/* <Cards  onClick = {() => {clickAndCheck(CarriedSkull)}} className = {randomizeOrder} name = "Carried Skull" src = "/imgs/CarriedSkull.png"/>
+
+<Cards onClick = {() => {clickAndCheck(DarkPichu)}} className = {randomizeOrder}  name = "Dark Pichu" src = "/imgs/DarkPichu.png"/>
+
+<Cards onClick = {() => {clickAndCheck(DualKnife)}} className = {randomizeOrder}  name = "Dual Knife" src = "/imgs/DualKnife.png"/>
+
+<Cards onClick = {() => {clickAndCheck(LuckyCat)}} className = {randomizeOrder}  name = "Lucky Cat" src = "/imgs/LuckyCat.png"/>
+
+<Cards onClick = {() => {clickAndCheck(SkullCat)}} className = {randomizeOrder}  name = "Skull Cat" src = "/imgs/SkullCart.png"/>
+
+<Cards onClick = {() => {clickAndCheck(TheCircleOfCats)}} className ={randomizeOrder}   name ="TheCircleOfCats" src = "/imgs/TheCircle.png"/> */}
